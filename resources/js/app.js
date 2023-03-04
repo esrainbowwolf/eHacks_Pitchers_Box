f20a74cb19a2669c5014ebc68b1d870f4b7f9f64
esrainbowwolf <46290536+esrainbowwolf@users.noreply.github.com>
2023-03-04 17:18:43 -0600

    Created Vuetify project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+// resources/app.js
+
+require("./bootstrap");
+
+import { createApp } from "vue";
+import Welcome from "./components/Welcome.vue";
+
+createApp({
+    components: {
+        Welcome,
+    },
+}).mount("#app");
